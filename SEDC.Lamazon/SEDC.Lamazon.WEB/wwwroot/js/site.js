70dea8406a18018560d5776fa5994a2c72165a06
Hristijan Petrovski <kiko-bt@hotmail.com>
2021-03-04 03:53:42 +0100

    dynamic year added

--- SEDC.Lamazon/SEDC.Lamazon.WEB/wwwroot/js/site.js
+++ SEDC.Lamazon/SEDC.Lamazon.WEB/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('year').innerHTML = new Date().getFullYear();
